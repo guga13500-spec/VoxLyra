626aa08b8f9bacfa5f7e265e979c5542b2b93743
guga13500-spec <guga13500@gmail.com>
2026-07-16 13:55:02 -0300

    Add Color.kt

---
+++ app/src/main/java/com/voxlyra/ui/theme/Color.kt
@@ -0,0 +1,24 @@
+package com.voxlyra.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Cores vibrantes e acessíveis
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650A4)
+val PurpleGrey40 = Color(0xFF625B71)
+val Pink40 = Color(0xFF7D5260)
+
+// Paleta VoxLyra - roxo/azul tecnológico
+val PrimaryDark = Color(0xFF6750A4)
+val PrimaryLight = Color(0xFF7C6FE8)
+val SecondaryDark = Color(0xFF03DAC6)
+val SecondaryLight = Color(0xFF018786)
+val BackgroundDark = Color(0xFF121212)
+val SurfaceDark = Color(0xFF1E1E1E)
+val ErrorRed = Color(0xFFCF6679)
+val OnPrimary = Color(0xFFFFFFFF)
+val AccentGreen = Color(0xFF4CAF50)
+val AccentOrange = Color(0xFFFF9800)
